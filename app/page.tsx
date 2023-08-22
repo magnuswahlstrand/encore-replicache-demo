32d8bcd65bb24eaf7d528b4f547fbbe48c735902
magnus <magnus.wahlstrand@gmail.com>
2023-08-22 22:27:41 +0200

    add shadn-ui and frodo's jobs

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Chat from "@/components/Chat"
+import {Task} from "@/components/Task";
+import {TaskList} from "@/components/TaskList";
 
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between py-10">
             <Chat/>
+            <TaskList/>
         </main>
     )
 }
---
+++ components/Task.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import {cn} from "@/lib/utils";
+
+
+type Props = {
+    title: string
+    completed: boolean
+};
+export const Task = (props: Props) => {
+    return (
+        <div>
+            <input type="checkbox" id="task_1" className="hidden"/>
+            <label className="flex items-center h-10 px-2 rounded cursor-pointer hover:bg-gray-100"
+                   htmlFor="task_1">
+					<span
+                        className={cn(
+                            "flex items-center justify-center w-5 h-5 text-transparent border-2 border-gray-300 rounded-full",
+                            {
+                                "bg-green-500 border-green-500 text-white": props.completed,
+                            }
+                        )}
+                    >
+						<svg className="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                             fill="currentColor">
+							<path fillRule="evenodd"
+                                  d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                                  clipRule="evenodd"/>
+						</svg>
+					</span>
+                <span className={cn("ml-4 text-sm", {
+                    "line-through": props.completed,
+                    "text-gray-400": props.completed,
+                })}> {props.title}</span>
+            </label>
+        </div>
+    )
+};
